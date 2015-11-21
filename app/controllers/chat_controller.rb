2792f064355bdb803979222b1152ba3f9711fde3
bradkim <bradkim@joongang.co.kr>
2015-11-21 02:58:45 +0000

    swp3_railsbasic

---
+++ app/controllers/chat_controller.rb
@@ -0,0 +1,14 @@
+class ChatController < ApplicationController
+    
+    def hello
+        @chats = Chat.all
+    end
+    
+    def submit
+        chat = Chat.new
+        chat.content = params["chat"]
+        chat.save
+        
+        redirect_to :action => "hello"
+    end
+end
